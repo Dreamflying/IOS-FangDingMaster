61452aa074d5aa65d02770d9704e84e970702ce5
code_darker <1522651962@qq.com>
2015-04-01 11:02:25 +0800

    增加 NSUserDefault 工具类

---
+++ FangDingMaster/Utils/UserDefaultUtils.h
@@ -0,0 +1,13 @@
+//
+//  UserDefaultUtils.h
+//  FangDingMaster
+//
+//  Created by Darker on 15/4/1.
+//  Copyright (c) 2015年 Darker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserDefaultUtils : NSObject
+
+@end
